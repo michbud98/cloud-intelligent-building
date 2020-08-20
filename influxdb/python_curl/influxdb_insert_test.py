dc48fd783dca82711e0a5eb9d16d8a843e832530
Michal Budik <Swiftblade1982@gmail.com>
2020-08-20 15:18:14 +0200

    Connection issues (#2)
    * Add connection verification to testing file before inserting into influxdb
    
    - Add method for checking connection to influxdb
    - Remove write?db=mydb from default http, path is added depanding on method
    - Argument -d now works with new variable database_name, script inserts into this database
    
    * Add changes from 3b271e32b to weather.py

--- influxdb/python_curl/influxdb_insert_test.py
+++ influxdb/python_curl/influxdb_insert_test.py
@@ -5,25 +5,67 @@ import getopt
 import random
 import time
 import requests
+from requests.exceptions import HTTPError, ConnectionError
+from subprocess import PIPE, Popen, check_output
 
 
-def insert_into_influxdb(http: str, data_string: str):
-    r = requests.post(http, data=data_string)
+def check_connection_with_influxdb(url_domain: str):
+    """
+    Checks if influxdb is available on http address taken from param
+
+    :param url_domain: url address of influxdb
+    :return: Nothing
+    :raises:
+        HTTPError: exception is raised for invalid http responses (status code > 200)
+        ConnectionError: ConnectionError exception is thrown in case of DNS failure,
+        refused connection or any other connection related issues
+    """
+    # http example http://localhost:8086/ping
+    print("Testing connection to {}".format(url_domain + str("ping")))
+    response = requests.get(url_domain + str("ping"))
+    print(f"Status code: {response.status_code}")
+    # If the response was successful, no Exception will be raised
+    response.raise_for_status()
+    print('Connection successful')
+
+
+def insert_into_influxdb(url_domain: str, database_name: str, data_string: str):
+    """
+    Runs insert operation in influxdb using curl command
+
+    :param url_domain: url address of influxdb
+    :param database_name: name of database to which we want to insert
+    :param data_string: data which we want to insert in db
+    :return: Nothing
+    :raises:
+        HTTPError: exception is raised for invalid http responses (status code > 200)
+        ConnectionError: ConnectionError exception is thrown in case of DNS failure,
+        refused connection or any other connection related issues
+    """
+    # http example 'http://localhost:8086/write?db=mydb'
+    r = requests.post(url_domain + str(f"write?db={database_name}"), data=data_string)
     print("{} sent to {}".format(data_string, r.url))
+
+
+# Check for Wi-Fi connection (takhle metoda na Macu nefunguje na raspberry ano)
+def check_wifi():
+    if check_output(['hostname', '-I']):
+        return True
+    else:
+        return False
+
 
 def print_help():
     print("influxdb_insert_test.py -s <sleep_time> -a <http_address>")
 
+
 def main(argv):
     sleep_time: int = 60
-    http: str = 'http://localhost:8086/write?db=mydb'
+    database_name_arg = "nodb"
+    url_domain_arg: str = 'http://localhost:8086/'
 
     try:
-        # if there are no arguments raises exception
-        if not argv:
-            # print("No arguments")
-            raise getopt.GetoptError("No arguments")
-        opts, args = getopt.getopt(argv, "hs:a:", ["help", "sleep=", "address="])
+        opts, args = getopt.getopt(argv, "hd:s:a:", ["help", "database=", "sleep=", "address=", "debug"])
     except getopt.GetoptError as argument_error:
         print(argument_error.msg)
         print_help()
@@ -34,17 +76,31 @@ def main(argv):
             sys.exit()
         if opt in ("-s", "--sleep"):
             sleep_time = int(arg)
+        if opt in ("-d", "--database"):
+            database_name_arg = arg
+        if opt in "--debug":
+            print("Setting log on debug")
         if opt in ("-a", "--address"):
-            http = arg
+            url_domain_arg = arg
 
     while True:
-        temperature = random.randint(-20, 40)
-        pressure = random.randint(-20, 40)
-        humidity = random.randint(-20, 40)
-        data_string = 'temperature value={} \n' \
-                      'pressure value={} \n' \
-                      'humidity value={}'.format(temperature, pressure, humidity)
-        insert_into_influxdb(http, data_string)
+        try:
+            temperature = random.randint(-20, 40)
+            pressure = random.randint(-20, 40)
+            humidity = random.randint(-20, 40)
+            data_string = 'temperature value={} \n' \
+                          'pressure value={} \n' \
+                          'humidity value={}'.format(temperature, pressure, humidity)
+            check_connection_with_influxdb(url_domain_arg)
+            insert_into_influxdb(url_domain_arg, database_name_arg, data_string)
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except ConnectionError as conn_err:
+            print(f'Connection error occurred: {conn_err}')
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+            sys.exit()
+
         print(f"Sleeping for {sleep_time} second")
         time.sleep(sleep_time)
 
--- influxdb/python_curl/weather.py
+++ influxdb/python_curl/weather.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 
 import sys
@@ -12,9 +11,13 @@ try:
 except ImportError:
     from smbus import SMBus
 
-from subprocess import PIPE, Popen
+from subprocess import PIPE, Popen, check_output
 import logging
 import requests
+from requests.exceptions import HTTPError, ConnectionError
+
+import platform  # For getting the operating system name
+import subprocess  # For executing a shell command
 
 # create logger with 'spam_application'
 logger = logging.getLogger()
@@ -38,14 +41,61 @@ bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
 
-def insert_into_influxdb(http: str, data_string: str):
-    r = requests.post(http, data=data_string)
+# Check for Wi-Fi connection (takhle metoda na Macu nefunguje na raspberry ano)
+def check_wifi():
+    """
+    Checks for wifi connection (works only on Raspberry PI)
+
+    :return: bool True/False depending on connection to wi-fi
+    """
+    if check_output(['hostname', '-I']):
+        return True
+    else:
+        return False
+
+
+def check_connection_with_influxdb(url_domain: str):
+    """
+    Checks if influxdb is available on http address taken from param
+
+    :param url_domain: url address of influxdb
+    :return: Nothing
+    :raises:
+        HTTPError: exception is raised for invalid http responses (status code > 200)
+        ConnectionError: ConnectionError exception is thrown in case of DNS failure,
+        refused connection or any other connection related issues
+    """
+    # http example http://localhost:8086/ping
+    logger.debug("Testing connection to {}".format(url_domain + str("ping")))
+    response = requests.get(url_domain + str("ping"))
+    logger.debug(f"Status code: {response.status_code}")
+    # If the response was successful, no Exception will be raised
+    response.raise_for_status()
+    logger.info(f'Connection to {url_domain} successful')
+
+
+def insert_into_influxdb(url_domain: str, database_name: str, data_string: str):
+    """
+    Runs insert operation in influxdb using curl command
+
+    :param url_domain: url address of influxdb
+    :param database_name: name of database to which we want to insert
+    :param data_string: data which we want to insert in db
+    :return: Nothing
+    :raises:
+        HTTPError: exception is raised for invalid http responses (status code > 200)
+        ConnectionError: ConnectionError exception is thrown in case of DNS failure,
+        refused connection or any other connection related issues
+    """
+    # http example 'http://localhost:8086/write?db=mydb'
+    r = requests.post(url_domain + str(f"write?db={database_name}"), data=data_string)
     logger.debug("{} sent to {}".format(data_string, r.url))
 
 
 def get_cpu_temperature() -> float:
     """
     Gets CPU temperature
+
     :return: Float CPU temperature value
     """
     process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
@@ -56,6 +106,7 @@ def get_cpu_temperature() -> float:
 def get_compensated_temperature() -> float:
     """
     Temporary method compensating heat from CPU
+
     :return: Float compensated temperature value
     """
     comp_factor = 2.25
@@ -77,13 +128,13 @@ def print_help():
 
 def main(argv):
     sleep_time: int = 60
-    http: str = 'http://192.168.88.140:8086/api/v2/write?bucket=mydb'
-    logger.debug("Starting script with arguments \n"
-                 "-s {} -a {}".format(sleep_time, http))
+    database_name_arg = "mydb"
+    url_domain_arg: str = 'http://192.168.88.140:8086/'
+
     try:
-        opts, args = getopt.getopt(argv, "hds:a:", ["help", "debug", "sleep=", "address="])
+        opts, args = getopt.getopt(argv, "hd:s:a:", ["help", "database=", "sleep=", "address=", "debug"])
     except getopt.GetoptError as argument_error:
-        logger.warning(argument_error.msg)
+        logger.error(argument_error.msg)
         print_help()
         sys.exit()
     for opt, arg in opts:
@@ -92,31 +143,49 @@ def main(argv):
             sys.exit()
         if opt in ("-s", "--sleep"):
             sleep_time = int(arg)
-        if opt in ("-d", "--debug"):
+        if opt in ("-d", "--database"):
+            database_name_arg = arg
+        if opt in "--debug":
             ch.setLevel(logging.DEBUG)
         if opt in ("-a", "--address"):
-            http = arg
+            url_domain_arg = arg
 
     logger.info("""weather.py - Print readings from the BME280 weather sensor.
        Press Ctrl+C to exit!
        """)
-    time.sleep(2)  # wait for temperature on sensor to raise
+
     while True:
-        temperature = get_compensated_temperature()
-        pressure = bme280.get_pressure()
-        humidity = bme280.get_humidity()
-        logger.info("""
-        Compensated_Temperature: {:05.2f} *C
-        Pressure: {:05.2f} hPa
-        Relative humidity: {:05.2f} %
-        """.format(temperature, pressure, humidity))
-        data_string = 'temperature value={} \n' \
-                      'pressure value={} \n' \
-                      'humidity value={}'.format(temperature, pressure, humidity)
-        insert_into_influxdb(http, data_string)
+        try:
+            temperature = get_compensated_temperature()
+            pressure = bme280.get_pressure()
+            humidity = bme280.get_humidity()
+            logger.info("""
+            Compensated_Temperature: {:05.2f} *C
+            Pressure: {:05.2f} hPa
+            Relative humidity: {:05.2f} %
+            """.format(temperature, pressure, humidity))
+            data_string = """
+            temperature value={}
+            pressure value={}
+            humidity value={}""".format(temperature, pressure, humidity)
+            if check_wifi():
+                logger.debug("Connected to wifi")
+                check_connection_with_influxdb(url_domain_arg)
+                insert_into_influxdb(url_domain_arg, database_name_arg, data_string)
+            else:
+                logger.info("No connection to wi-fi")
+                # TODO Add saving to local file
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except ConnectionError as conn_err:
+            print(f'Connection error occurred: {conn_err}')
+        except Exception as err:
+            logger.error(f'Other error occurred: {err}')
+            sys.exit()
+
+        logger.info(f"Sleeping for {sleep_time} second")
         time.sleep(sleep_time)
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
-
